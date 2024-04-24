6bb9078200e191820eed54686ecb5c6a68c0687f
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2024-04-24 13:13:33 -0400

    First commit

---
+++ src/layout/Landing.jsx
@@ -0,0 +1,25 @@
+import  './Landing.css'
+
+const Landing = () => {
+  return (
+    <>
+    <section className="Ldn-section">
+        <div className="opener">
+            <div style={{display:"flex",flexDirection:"row"}}><h1>Health</h1><h1 style={{color:"#05200E"}}> Vault</h1></div>
+            <p>Your number one smart health app</p>
+        </div>
+        <div className="form">
+            <span className='desc'>Welcome back!</span>
+            <a href="">Log in</a>
+
+            <span className='desc'>First time?</span>
+            <a href="">Sign Up</a>
+        </div>
+      
+        
+    </section>
+    </>
+  )
+}
+
+export default Landing
---
+++ src/layout/home.jsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/ui/navbar"
+const Home = () => {
+  return (
+    <>
+    <div className="home-container">
+        <section>
+            <Navbar/>
+        </section>
+        <section>
+            {/* Container goes here*/}
+            hello
+        </section>
+    </div>
+    </>
+  )
+}
+
+export default Home
