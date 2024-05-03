1991015dc0dfe2de7fb85f3b59e7a94f6b9688b5
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2024-05-03 13:08:32 -0400

    Finished the main components

--- Backend/user.py
+++ Backend/user.py
@@ -2,7 +2,7 @@ from DB import *
 
 def get_user_by_id(id):
     pass
-def add_task_touser(task_id,user_id):
+def add_task_to_user(task_id,user_id):
     return User.update_one({"_id":user_id},{"$push":{"task_id":task_id}})
 
 def add_fit_goal(user_id,goal):
